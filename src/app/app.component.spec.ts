116476691d37814587a02996cd699b5798581d8c
Plazma06 <example@gmail.com>
2025-03-20 16:16:56 +0300

    new style

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,14 @@ describe('AppComponent', () => {
   it(`should have the 'test-mini-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-mini-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test-mini-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, test-mini-app'
+    );
   });
 });
